eb88b69bac3200d08c89c94db5095c9076dd0ba2
Achyut Gupta <guptaachyut15@gmail.com>
2023-12-23 12:06:54 +0530

    backend architecture

--- node-server/index.js
+++ node-server/index.js
@@ -4,19 +4,31 @@ const app = express();
 const server = http.createServer(app);
 
 const { Server } = require("socket.io");
-const io = new Server(server);
+const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:5500"];
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 const users = {};
 
 io.on("connection", (socket) => {
   socket.on("user-joined", (name) => {
     users[socket.id] = name;
-    console.log("************");
     socket.broadcast.emit("user-joined", name);
   });
 
   socket.on("send", (msg) => {
     socket.broadcast.emit("send", { name: users[socket.id], msg: msg });
+  });
+
+  socket.on("disconnect", () => {
+    const disconnectedUser = users[socket.id];
+    socket.broadcast.emit("user-left", disconnectedUser);
+    delete users[socket.id];
   });
 });
 
