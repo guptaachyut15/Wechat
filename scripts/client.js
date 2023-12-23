d1c50623f5078c7b147e770c7adc683b7561a153
Achyut Gupta <guptaachyut15@gmail.com>
2023-12-23 12:13:58 +0530

    added notification sound

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,47 @@
-var socket = io();
+var socket = io("http://localhost:8080");
+const audio = new Audio("../ting.mp3");
 
-socket.emit("user-joined", Abcd);
+let form = document.getElementById("send-container");
+let messageInp = document.getElementById("messageInp");
+let messageContainer = document.getElementById("chatbox");
+
+const addMessage = function (message, position) {
+  let messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  if (position === "left") {
+    messageElement.classList.add("received");
+  } else if (position === "right") {
+    messageElement.classList.add("sent");
+  }
+  messageContainer.append(messageElement);
+};
+
+let userName = prompt("Enter your name");
+while (!userName) {
+  userName = prompt("Enter a valid name");
+}
+
+socket.emit("user-joined", userName);
+
+socket.on("user-joined", (name) => {
+  addMessage(`${name} joined the chat`, "center");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let message = messageInp.value;
+  addMessage(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInp.value = "";
+});
+
+socket.on("send", (messageObject) => {
+  addMessage(`${messageObject.name}: ${messageObject.msg}`, "left");
+  audio.play();
+});
+
+socket.on("user-left", (name) => {
+  addMessage(`${name} left`, "center");
+});
